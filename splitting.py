05127744ec8aadc8160a4e96d61ac5ee0db98071
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-12-25 16:59:58 +0100

    corrections + time

--- splitting.py
+++ splitting.py
@@ -6,6 +6,7 @@ Created on Sat Dec 25 09:12:44 2021
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # Entrace in the circular crown with internal radius r and external radius R
 def entrance(x,y,r,R):
@@ -35,26 +36,21 @@ def u2(x,y):
     return Q*y/(2*np.pi*(np.power(x,2)+np.power(y,2)))
 
 
-R = np.arange(8.5,0.5,-0.5) # vector of external radius of my circular crowns
-r = R-0.5 
+R = np.arange(8.5,0.5,-1.5) # vector of external radius of my circular crowns
+r = R-1.5 
 r[-1] = 0 # vector of internal radius of my circular crowns
-rho = np.zeros(len(R))
-for i in range(len(R)-1):
-    rho[i+1] = np.random.uniform(r[i],R[i],size=(1,))
-theta = np.random.uniform(0,2*np.pi,size = len(R))
 P = np.zeros(len(R)) # vector to store the probabilities P(tau_j <= T | tau_(j-1) <= T)
-X = rho * np.cos(theta)
-Y = rho * np.sin(theta)
-X[0] = X0
-Y[0] = Y0
+iters = 100*np.ones(len(R)) # number of subpaths studied inside each circular crown
 
 def rw(K,X0,Y0,r,R):
     
     finished_mc=0
-    finished_av=0
     x_mc = X0
     y_mc = Y0
-    
+    xs = []
+    ys = []
+    xs.append(X0)
+    ys.append(Y0)
     for i in range(K):   
         Z1 = np.float(np.random.normal(size = 1))
         Z2 = np.float(np.random.normal(size = 1))       
@@ -63,31 +59,50 @@ def rw(K,X0,Y0,r,R):
         u2_old = u2(x_mc,y_mc)
         x_mc = x_mc + u1_old*dt + sigma * np.sqrt(dt)*Z1
         y_mc = y_mc + u2_old*dt + sigma * np.sqrt(dt)*Z2
+        xs.append(x_mc)
+        ys.append(y_mc)
         if (entrance(x_mc,y_mc,r,R)) : 
             finished_mc=1
            
-    return finished_mc
+    return finished_mc,xs,ys
 
-for j in range(len(R)):
-    Z = 0
-    for k in range(int(N)):    
-        Z = Z + rw(K,X[j],Y[j],r[j],R[j])
-    Z = Z / N
-    P[j] = Z
+# ax = plt.gca()
+# circle1 = plt.Circle((0, 0), R[-1], color='darkturquoise', fill = True)
+# ax.add_patch(circle1)
+# for i in range(len(R)-1):
+#     circle = plt.Circle((0, 0), R[i], color='green', fill = False)
+#     ax.add_patch(circle)
+# plt.xlim(-9,9)
+# plt.ylim(-9,9)
+# ax.set_aspect('equal', adjustable='box')
+# ax.plot(X0,Y0, 'ro')
+
+X0s_old = [X0]
+Y0s_old = [Y0]
+
+start = time.time()
+
+for j in range(len(R)): # for each level
+    K_r = int(iters[j])
+    X0s_new = []
+    Y0s_new = []
+    for n in range(len(X0s_old)): # for each initial point of the previous state
+        for k in range(K_r): # K_r subpaths
+            # Storage of the subpath
+            xs = []
+            ys = []
+            hit,xs,ys = rw(K,X0s_old[n],Y0s_old[n],r[j],R[j])
+            #ax.plot(xs,ys)
+            if(hit == 1):
+                #ax.plot(xs[-1],ys[-1],'bo')
+                X0s_new.append(xs[-1]) # store new intial point for next state
+                Y0s_new.append(ys[-1])
+                P[j] = P[j] + 1
+    P[j] = P[j] / (K_r * len(X0s_old))
+    X0s_old = X0s_new
+    Y0s_old = Y0s_new
     
-
-ax = plt.gca()
-circle1 = plt.Circle((0, 0), R[-1], color='darkturquoise', fill = True)
-ax.add_patch(circle1)
-for i in range(len(R)-1):
-    circle = plt.Circle((0, 0), R[i], color='green', fill = False)
-    ax.add_patch(circle)
-plt.xlim(-9,9)
-plt.ylim(-9,9)
-ax.set_aspect('equal', adjustable='box')
-ax.plot(X,Y,'b*')
-ax.plot(X[0],Y[0],'bo')
-ax.plot(X[-1],Y[-1], 'ro')
-
+end = time.time()
+print("Simulation time: ", end - start, "s; in minutes: ",(end-start)/60) 
 res = np.prod(P)
 print(res)
