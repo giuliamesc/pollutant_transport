7221a5b644f54cb545b467cdd5224d6e1a1516e2
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-12-25 16:59:36 +0100

    Update variance_reduction.py

--- variance_reduction.py
+++ variance_reduction.py
@@ -11,19 +11,23 @@ import numpy as np
 import scipy.stats as st
 import parameters
 
-N=10000 #Number of samples
-N2=int(N/2) #Number of samples for AV
-Z_mc=np.zeros(N)
-Z_mc2=np.zeros(N2)
-Z_av=np.zeros(N2)
+
+# Options and parameters
+N=10000 # Number of samples
+N2=int(N/2) # Number of samples for AV
 dt = 1e-2
 T = 1
 Q = 1
 sigma = 2
-maxit = 1000
+# maxit = 1000
 K = int(T/dt) # iteration corresponding to the desired stopping time
 alpha = 0.05
 C_alpha=st.norm.ppf(1-alpha/2)
+
+# Vectors for storage
+Z_mc=np.zeros(N)
+Z_mc2=np.zeros(N2)
+Z_av=np.zeros(N2)
 
 # Velocity functions
 def u1(x,y): 
@@ -32,7 +36,7 @@ def u1(x,y):
 def u2(x,y):
     return Q*y/(2*np.pi*(np.power(x,2)+np.power(y,2)))
 
-
+# Killing boundary
 def killing_boundary(x,y):
     # This function receives the coordinate of a point and returns true if that coordinate is inside the well
     if(np.sqrt(np.power(x,2)+np.power(y,2)) <= 1) : return True
@@ -40,7 +44,7 @@ def killing_boundary(x,y):
 
 # Defining the random walk for the CMC and the AV 
 #----------------------------------------------------
-def rw(maxit,K):
+def rw(K):
     
     finished_mc=0
     finished_av=0
@@ -49,8 +53,7 @@ def rw(maxit,K):
     x_av = parameters.X0
     y_av = parameters.Y0
     
-    for i in range(maxit):   
-        
+    for i in range(K):   
         Z1 = np.float(np.random.normal(size = 1))
         Z2 = np.float(np.random.normal(size = 1))       
         # Moves crude MC
@@ -58,19 +61,20 @@ def rw(maxit,K):
         u2_old = u2(x_mc,y_mc)
         x_mc = x_mc + u1_old*dt + sigma * np.sqrt(dt)*Z1
         y_mc = y_mc + u2_old*dt + sigma * np.sqrt(dt)*Z2
-        if (killing_boundary(x_mc,y_mc)) : finished_mc=1
-         # Moving AV with the same random realization         
+        if (killing_boundary(x_mc,y_mc)) : 
+            finished_mc=1
+        # Moving AV with the same random realization         
         u1_old = u1(x_av,y_av)
         u2_old = u2(x_av,y_av)
         x_av = x_av + u1_old*dt - sigma * np.sqrt(dt)*Z1
         y_av = y_av + u2_old*dt - sigma * np.sqrt(dt)*Z2
-        if (killing_boundary(x_av,y_av)) : finished_av=1
-
+        if (killing_boundary(x_av,y_av)) : 
+            finished_av=1
            
     return finished_mc,finished_av
 
 for i in range(int(N)):    
-    Z_mc[i],_=rw(maxit,K)
+    Z_mc[i],_=rw(K)
 
 
 mean_cmc=np.mean(Z_mc)
@@ -79,7 +83,7 @@ ci_cmc=C_alpha*np.sqrt(var_cmc)/np.sqrt(N)
 
 # Compute the AV estimator 
 for i in range(int(N2)):    
-    Z_mc2[i],Z_av[i]=rw(maxit,K)
+    Z_mc2[i],Z_av[i]=rw(K)
 
 Y_av=0.5*(Z_mc2+Z_av)
 mean_av=np.mean(Y_av)
