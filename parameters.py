314e4514491774f6ce1b03d25b2b20ac0be014e8
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2022-01-07 11:19:06 +0100

    two more test cases

--- parameters.py
+++ parameters.py
@@ -11,13 +11,21 @@ testcase = 2
 # Temporal step
 dt = 1e-2
 
+if testcase == 1:
+    # Starting point
+    X0 = 1.2
+    Y0 = 1.1
+    # Plot limits
+    xl = 3
+    yl = 3
+
 if testcase == 2:
     # Starting point
     X0 = 2.5
     Y0 = 2.5
     # Plot limits
-    xl = 5
-    yl = 5
+    xl = 5.5
+    yl = 5.5
 
 if testcase == 3:
     # Starting point
@@ -26,3 +34,19 @@ if testcase == 3:
     # Plot limits
     xl = 7
     yl = 7
+    
+if testcase == 4:
+    # Starting point
+    X0 = -1.2
+    Y0 = 1.1
+    # Plot limits
+    xl = 3
+    yl = 3
+    
+if testcase == 5:
+    # Starting point
+    X0 = 7
+    Y0 = 8
+    # Plot limits
+    xl = 10
+    yl = 10
