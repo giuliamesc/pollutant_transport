a1eaf8506411cb5d2969a2d3f61ed334d9073f46
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2022-01-17 17:49:50 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -36,7 +36,7 @@ if testcase == 3:
     xl = 7
     yl = 7
     # Tolerance
-    tol = 1e-3
+    tol = 1e-4
     
 if testcase == 4:
     # Starting point
@@ -46,7 +46,7 @@ if testcase == 4:
     xl = 3
     yl = 3
     # Tolerance
-    tol = 1e-4
+    tol = 1e-2
     
 if testcase == 5:
     # Starting point
